2d73c4b365db29d79f68578644a45d501b6bab5a
evilnop <evilnops@gmail.com>
2018-09-14 11:37:56 +0800

    feat(bean):register beans through `beanDefinitionDSL` in a functional way using lambdas

---
+++ src/main/kotlin/io/github/eureka/bean/Beans.kt
@@ -0,0 +1,16 @@
+package io.github.eureka.bean
+
+import io.github.eureka.web.Routes
+import io.github.eureka.web.UserHandler
+import org.springframework.context.support.beans
+import org.springframework.web.reactive.function.server.RouterFunctions
+
+fun beans() = beans {
+
+    bean<UserHandler>()
+    bean<Routes>()
+
+    bean("webHandler") {
+        RouterFunctions.toWebHandler(ref<Routes>().router())
+    }
+}
