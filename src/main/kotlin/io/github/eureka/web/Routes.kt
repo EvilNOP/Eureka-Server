c2f56ef2307775c3d27339b31e5c4a1f7d7fb890
EvilNOP <evilnops@gmail.com>
2018-10-02 22:51:17 +0800

    chore(web):extract the import

--- src/main/kotlin/io/github/eureka/web/Routes.kt
+++ src/main/kotlin/io/github/eureka/web/Routes.kt
@@ -1,12 +1,13 @@
 package io.github.eureka.web
 
 import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.router
 
 class Routes(
     private val userHandler: UserHandler
 ) {
 
-    fun router() = org.springframework.web.reactive.function.server.router {
+    fun router() = router {
         accept(MediaType.APPLICATION_JSON_UTF8).nest {
             GET("/users", userHandler::findAll)
         }
