820403e6bcbca825c76914664275d8cdc9df9e5e
EvilNOP <evilnops@gmail.com>
2018-10-07 16:29:55 +0800

    chore(eureka):add annotation `SpringBootApplication`

--- src/main/kotlin/io/github/eureka/Eureka.kt
+++ src/main/kotlin/io/github/eureka/Eureka.kt
@@ -1,14 +1,12 @@
 package io.github.eureka
 
 import io.github.eureka.bean.beans
-import org.springframework.boot.SpringBootConfiguration
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @EnableEurekaServer
-@SpringBootConfiguration
-@EnableAutoConfiguration
+@SpringBootApplication
 class Eureka
 
 fun main(args: Array<String>) {
