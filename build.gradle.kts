759628d2226bea814dc26620c2119fc7fff1fd8b
EvilNOP <evilnops@gmail.com>
2018-12-10 14:26:20 +0800

    build($root):upgrade the version of `kotlin` and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,13 +9,14 @@ repositories {
     mavenCentral()
 }
 
+val ktlintVersion = "0.29.0"
+
 // Tweak to be sure to have compiler and dependency versions the same
 extra["kotlin.version"] = plugins.getPlugin(KotlinPluginWrapper::class.java).kotlinPluginVersion
 
 plugins {
-    val kotlinterVersion = "1.16.0"
-    val kotlinVersion = "1.2.70"
-    val springBootVersion = "2.0.5.RELEASE"
+    val kotlinVersion = "1.3.10"
+    val springBootVersion = "2.0.7.RELEASE"
     val dependencyManagementVersion = "1.0.6.RELEASE"
 
     application
@@ -27,8 +28,6 @@ plugins {
 
     id("org.springframework.boot") version springBootVersion
     id("io.spring.dependency-management") version dependencyManagementVersion
-
-    id("org.jmailen.kotlinter") version kotlinterVersion
 }
 
 application {
@@ -37,6 +36,39 @@ application {
 
 group = "io.github"
 version = "0.0.1-SNAPSHOT"
+
+val ktlint by configurations.creating
+
+val lint by task<JavaExec> {
+    group = "ktlint"
+    description = "Check Kotlin code style"
+    classpath = configurations.getByName("ktlint")
+    main = "com.github.shyiko.ktlint.Main"
+
+    setArgs(listOf("src/**/*.kt"))
+}
+
+val format by task<JavaExec> {
+    group = "ktlint"
+    description = "Fix Kotlin code style deviations"
+    classpath = configurations.getByName("ktlint")
+    main = "com.github.shyiko.ktlint.Main"
+
+    setArgs(listOf("-F", "src/**/*.kt"))
+}
+
+val installGitHook by task<JavaExec> {
+    group = "ktlint"
+    description = "Install git hook to automatically check files for style violations on commit"
+    classpath = configurations.getByName("ktlint")
+    main = "com.github.shyiko.ktlint.Main"
+
+    setArgs(listOf("--install-git-pre-commit-hook"))
+}
+
+tasks.getByName("check") {
+    setDependsOn(listOf(tasks.getByName("lint")))
+}
 
 tasks.withType<Jar> {
     baseName = "eureka"
@@ -56,7 +88,7 @@ configure<JavaPluginConvention> {
 }
 
 dependencyManagement {
-    val springCloudVersion = "Finchley.SR1"
+    val springCloudVersion = "Finchley.SR2"
 
     imports {
         mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
@@ -67,9 +99,13 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
 
+    ktlint("com.github.shyiko:ktlint:$ktlintVersion")
+
     compile("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
     compile("org.springframework:spring-webflux")
     compile("io.projectreactor.ipc:reactor-netty")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
+
+inline fun <reified T : Task> task(noinline configuration: T.() -> Unit) = tasks.creating(T::class, configuration)
